341d421c4de63cd39f7e87353bf4bb06c29bda55
GGupta03 <gaurav.gupta26102003@gmail.com>
2026-02-03 15:55:42 +0530

    Updated Add role-based authorization and session guard using custom MVC filter

--- E-Insurance_App/obj/Debug/net8.0/E-Insurance_App.AssemblyInfo.cs
+++ E-Insurance_App/obj/Debug/net8.0/E-Insurance_App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Insurance_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24b8554a8da501bc3d4d34000f93c5042afc5775")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d8173aed3867bc76997e1846bdafec2a5f04994")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Insurance_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Insurance_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
