976e40a45d3a3443cd584167e0d2ddddf2d0b045
GGupta03 <gaurav.gupta26102003@gmail.com>
2026-02-07 14:43:31 +0530

    Enhanced My Policies Dashboard for customers, payment and invoice download

--- E-Insurance_App/obj/Debug/net8.0/E-Insurance_App.AssemblyInfo.cs
+++ E-Insurance_App/obj/Debug/net8.0/E-Insurance_App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Insurance_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06c333ce8d727dca4fba338a3c49a15b390c7b27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09acb309955479fb2f6eb07bac85423cdcb5fca7")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Insurance_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Insurance_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
