30e5420e27c2d16dcf27fcfac906120981127086
GGupta03 <gaurav.gupta26102003@gmail.com>
2026-02-09 16:01:58 +0530

    Profile sections for the Entities.

--- E-Insurance_App/Controllers/HomeController.cs
+++ E-Insurance_App/Controllers/HomeController.cs
@@ -1,12 +1,16 @@
 using E_Insurance_App.Filters;
+using E_Insurance_App.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace E_Insurance_App.Controllers
 {
     public class HomeController : Controller
     {
-        public HomeController()
+        private readonly UserRepository _userRepository;
+
+        public HomeController(UserRepository userRepository)
         {
+            _userRepository = userRepository;
         }
 
         public IActionResult Index()
@@ -16,6 +20,19 @@ namespace E_Insurance_App.Controllers
 
         [AuthorizeRole]
         public IActionResult Profile()
+        {
+            var userId = HttpContext.Session.GetInt32("UserId");
+            if (userId == null)
+                return RedirectToAction("Login", "Account");
+
+            var user = _userRepository.GetUserById(userId.Value);
+            if (user == null)
+                return RedirectToAction("Login", "Account");
+
+            return View(user);
+        }
+
+        public IActionResult Privacy()
         {
             return View();
         }
--- E-Insurance_App/obj/Debug/net8.0/E-Insurance_App.AssemblyInfo.cs
+++ E-Insurance_App/obj/Debug/net8.0/E-Insurance_App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Insurance_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8590f857277cdcb7d376b75496ce2c304a58c7a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11883acf4338ebc3803b6292914907b5eabdd6dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Insurance_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Insurance_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
